51c22ea98c29da558dd676036f9f4713625382a1
koga1020 <koga.shozo1020@gmail.com>
2022-07-03 22:11:15 +0900

    update application

--- lib/smart_cell_iex_syntax/application.ex
+++ lib/smart_cell_iex_syntax/application.ex
@@ -1,19 +1,13 @@
 defmodule SmartCellIexSyntax.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: SmartCellIexSyntax.Worker.start_link(arg)
-      # {SmartCellIexSyntax.Worker, arg}
-    ]
+    Kino.SmartCell.register(SmartCellIexSyntax)
+    children = []
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SmartCellIexSyntax.Supervisor]
     Supervisor.start_link(children, opts)
   end
