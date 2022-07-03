d60f0a862a2235abe549b7fdb6c08109ea34d709
koga1020 <koga.shozo1020@gmail.com>
2022-07-03 22:07:38 +0900

    init project

---
+++ lib/smart_cell_iex_syntax.ex
@@ -0,0 +1,18 @@
+defmodule SmartCellIexSyntax do
+  @moduledoc """
+  Documentation for `SmartCellIexSyntax`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SmartCellIexSyntax.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/smart_cell_iex_syntax_test.exs
@@ -0,0 +1,8 @@
+defmodule SmartCellIexSyntaxTest do
+  use ExUnit.Case
+  doctest SmartCellIexSyntax
+
+  test "greets the world" do
+    assert SmartCellIexSyntax.hello() == :world
+  end
+end
